81e17908a77b444b92339facaa308b8000378da5
Constantine Panfilov <ignalion@gmail.com>
2014-11-18 15:48:25 +0000

    Small fixes in config/base.py

--- app/config/base.py
+++ app/config/base.py
@@ -2,16 +2,20 @@ import os
 
 from app import app
 
+
 class BaseConfig(object):
-    BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+    BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                           os.path.pardir))
     SECRET_KEY = '72f7cdbaae694f86bd7e219e8421a94d'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'bookshelf.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
+                                                          'bookshelf.db')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
     DEBUG = False
-    
+
     APP_FOLDER = os.path.split(os.path.split(BASEDIR)[0])[0]
     TEMPLATES_FOLDER = os.path.join(APP_FOLDER, 'app', 'templates')
 
     USERDATA_DIRECTORY_NAME = "user_data"
     USERDATA_DIRECTORY = os.path.join(app.static_folder,
                                       USERDATA_DIRECTORY_NAME)
+    VERSION = '1.0.0'
