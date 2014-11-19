308a775b55dcf703fbbfd76f0f58d45049fb2db3
Constantine Panfilov <ignalion@gmail.com>
2014-11-19 11:13:24 +0000

    Prepared to release

--- app/config/base.py
+++ app/config/base.py
@@ -7,8 +7,11 @@ class BaseConfig(object):
     BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                            os.path.pardir))
     SECRET_KEY = '72f7cdbaae694f86bd7e219e8421a94d'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
-                                                          'bookshelf.db')
+    if os.environ.get('DATABASE_URL') is None:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR,
+                                                              'bookshelf.db')
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
     DEBUG = False
 
@@ -18,4 +21,4 @@ class BaseConfig(object):
     USERDATA_DIRECTORY_NAME = "user_data"
     USERDATA_DIRECTORY = os.path.join(app.static_folder,
                                       USERDATA_DIRECTORY_NAME)
-    VERSION = '1.0.0'
+    VERSION = '1.1.0'
